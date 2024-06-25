599baeac8171e5567a93c0059db991f2c3203719
Euller Lourenço <eullerlou321@gmail.com>
2024-06-24 22:05:42 -0300

    first commit

---
+++ src/main/java/one/dio/gof/repository/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package one.dio.gof.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import one.dio.gof.model.Endereco;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
